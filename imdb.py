a633a8df56dbd882acec8716ef97c40022b20fbf
Francisco Sanz <frasanz@bifi.es>
2019-04-22 22:49:04 +0200

    Added layer to imdb

--- imdb.py
+++ imdb.py
@@ -44,6 +44,7 @@ model = models.Sequential()
 
 model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
 model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
 #compile the model
