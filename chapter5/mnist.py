2047c039082f7addc956947cfa145b26161e3c27
Francisco Sanz <frasanz@bifi.es>
2019-04-23 17:08:56 +0200

    Mnist with conv2D

--- chapter5/mnist.py
+++ chapter5/mnist.py
@@ -1,23 +1,42 @@
-import tensorflow as tf
-import matplotlib.pyplot as plt
-mnist = tf.keras.datasets.mnist
+from keras import models
+from keras import layers
+from keras.datasets import mnist
+from keras.utils import to_categorical
 
-(x_train, y_train),(x_test,y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test/255.0
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28,28)),
-    tf.keras.layers.Dense(512, activation = tf.nn.relu),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(10,activation=tf.nn.softmax)
-    ])
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
+train_images = train_images.reshape((60000, 28, 28, 1))
+train_images = train_images.astype('float32') / 255
+
+test_images = test_images.reshape((10000, 28, 28, 1))
+test_images = test_images.astype('float32')/255
+
+train_labels = to_categorical(train_labels)
+test_labels  = to_categorical(test_labels)
+
+
+model = models.Sequential()
+model.add(layers.Conv2D(32,(3,3), activation='relu', input_shape = (28,28,1)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64,(3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64,(3,3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+model.summary()
+
+model.compile(optimizer = 'rmsprop',
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
-history = model.fit(x_train, y_train, epochs=5)
-model.evaluate(x_test, y_test)
+model.fit(train_images, train_labels, epochs=5, batch_size=64)
 
-plt.xlabel('Epoch Number')
-plt.ylabel('Loss Magnitude')
-plt.plot(history.history['loss'])
-plt.show()
+test_loss, test_acc = model.evaluate(test_images,test_labels)
+print(test_acc)
+
+
+#plt.xlabel('Epoch Number')
+#plt.ylabel('Loss Magnitude')
+#plt.plot(history.history['loss'])
+#plt.show()
